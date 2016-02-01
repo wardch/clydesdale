e1df212376e4f961e9374fb8c7c9e617933fa88d
wardch <charlieward18@gmail.com>
2016-01-31 17:46:17 -0800

    added intial static pages

--- clydesdale/app/controllers/static_pages_controller.rb
+++ clydesdale/app/controllers/static_pages_controller.rb
@@ -1,5 +1,16 @@
 class StaticPagesController < ApplicationController
   def home
-    
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def services
+  end
+
+  def why_us
   end
 end
--- clydesdale/config/routes.rb
+++ clydesdale/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resource :static_pages
   root 'static_pages#home'
+
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'services' => 'static_pages#services'
+  get 'why_us' => 'static_pages#why_us'
 end
